65a9a32d1f77d8d1425c9005b1622c4b8604e267
spanow <manil.masdoua@dauphine.eu>
2021-02-02 14:09:54 +0100

    1 file deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
-import { CartService } from "./cart.service";
-import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
@@ -19,8 +17,6 @@ import { ShippingComponent } from './shipping/shipping.component';
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
-      { path: 'shipping', component: ShippingComponent },
-
     ])
   ],
   declarations: [
@@ -29,10 +25,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    ShippingComponent
   ],
   bootstrap: [AppComponent],
-  providers: [CartService]
 })
 export class AppModule {}
 
